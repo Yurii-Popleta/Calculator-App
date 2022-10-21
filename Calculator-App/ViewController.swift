9b0153e9bec05ccd9c3f24384c50f778695a0c1e
Yurii Popleta <yurka279@gmail.com>
2022-10-21 18:24:06 +0200

    Initial Commit

---
+++ Calculator-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator-App
+//
+//  Created by Admin on 21/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
